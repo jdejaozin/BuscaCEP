60e1bdaf06ca3d0b1536cbd2c1a31abd5455da5c
jp_te <jp_te@DESKTOP-8JR9HVI>
2022-07-15 20:34:38 -0300

    Add project files.

---
+++ BuscaCEP/BuscaCEP/ViewModel/BuscaCepViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuscaCEP.ViewModel
+{
+    class BuscaCepViewModel : ViewModelBase
+    {
+        private string _CEP;
+        public string CEP
+        {
+            get => _CEP;
+            set
+            {
+                _CEP = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ BuscaCEP/BuscaCEP/ViewModel/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BuscaCEP.ViewModel
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /*protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged is null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }*/
+
+        protected void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged is null)
+                return;
+
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
